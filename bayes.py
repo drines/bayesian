81b6958f68f6dc8fe78d86484c8506b1d2efd7b6
Daniel Rines <drines@gmail.com>
2019-05-20 15:02:51 -0700

    feat: added the bayes function
    Wrote a function called: bayes that accepts the pior probabilities
    that were provided by the user via the command line. The joint,
    normalizer and then the posterior probability are calculated within
    the bayes function and the posterior is returned.
    
    The returned value is output in the main function, which is also
    the calling function.

--- bayes.py
+++ bayes.py
@@ -36,21 +36,61 @@ def get_input_args():
      parse_args() -data structure that stores the command line arguments
     """
     # Create Parse using ArgumentParser
-    parser = argparse.ArgumentParser(description='Calcs Bayesian posterior prob P(A | B).')
+    parser = argparse.ArgumentParser(description='Calcs Bayesian ' +
+                                     'posterior prob P(A | B).')
     # Create the command line arguments as mentioned above
-    parser.add_argument('--pA,
+    parser.add_argument('--pA',
                         type=float,
-                        default= 0.3,
+                        default=0.3,
                         help='P(A): (default: 0.3).')
     parser.add_argument('--pBgivenA',
                         type=float,
-                        default= 0.7',
+                        default=0.7,
                         help='P(B | A): (default: 0.7).')
     parser.add_argument('--pnotBgivennotA',
                         type=float,
-                        default= 0.9,
+                        default=0.9,
                         help='P(notA | notB): (default: 0.9).')
 
     # Return the parsed arguments back to the calling function
     return parser.parse_args()
 
+
+def bayes(p_A, p_B_given_A, p_notB_given_notA):
+    """
+    Calculates the posterior Bayesian probability given the prior
+    conditional probabilities as provided from the cmd line.
+    INPUT: p(A), p(B | A), p(notB | notA)
+    """
+    # Calculate P(notA) & P(B | notA) probabilities using the prior
+    p_notA = abs(1 - p_A)
+    p_B_given_notA = abs(1 - p_notB_given_notA)
+
+    # Calculate the joint probabilities P(A, B) & P(notA, B)
+    p_A_B = p_A * p_B_given_A
+    p_notA_B = p_notA * p_B_given_notA
+
+    # Calculate the normalizer
+    p_B = p_A_B + p_notA_B
+
+    # Calculate the posterior probability and return the value
+    posterior = p_A * p_B_given_A / p_B
+    return posterior
+
+
+def main():
+    """
+    The main function for call the bayes function.
+    PARAMETERS: None
+    RETURNS:    None
+    """
+    # parse in the input arguments
+    in_args = get_input_args()
+
+    # call the posterior and print the result to the terminal
+    posterior = bayes(in_args.pA, in_args.pBgivenA, in_args.pnotBgivennotA)
+    print('The posterior is: ' + str(posterior))
+
+# Call to main function to run the program
+if __name__ == "__main__":
+    main()
